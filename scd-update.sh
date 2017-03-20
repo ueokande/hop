98757414b46f896f352acf101cf2d30d8c8f9d73
Shin'ya UEOKA <ueokande@i-beam.org>
2017-03-20 09:56:07 +0900

    Fix scd-update to check directory

--- scd-update.sh
+++ scd-update.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 printf "" >"$HOME"/.scdrc
+
 while test $# != 0; do
+  if ! [ -d $1 ]; then
+    echo 2>&1 "$0: '$1': No such file or directory"
+    shift
+    continue
+  fi
+
   for dir in $1/*; do
     echo "$(basename "$dir")=$dir" >>"$HOME"/.scdrc
   done
